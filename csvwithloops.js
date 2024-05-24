e1613357ecfba4e0bdf2c40618440f82a8b42d8b
jlounsb1 <jefflounsbury@gmail.com>
2024-05-24 09:16:02 -0400

    added comments and cleaned up csv exercise

--- csvwithloops.js
+++ csvwithloops.js
@@ -5,19 +5,6 @@ let everything = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\
 
 let allTheThings ="Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
 
-// let wholeString = 'asdh,bfhjas,dvfhjavsdhjf. asdfa,sfasdf, asdffff'
-// let cell1;
-// let cell2;
-// for (const i of wholeString) {
-//     if(i !==',') {
-//         cell1+=i;
-//     } else 
-//         cell2+=i;
-    
-// }
-
-// console.log(cell1, cell2)
-
 
 let everythingArray = everything.split('\n');
 
@@ -26,7 +13,11 @@ console.log(everythingArray);
 //doing this by using built in methods and arrays
 
 
+/*
 
+This is able to grab the first word, but I could not send it to variables without manually making up to 20 cells.
+I wanted to make a loop to assign variables but I couldn't get the variables to assing in the scope of the loop.
+*/
 let cell1 = '';
 let cell2 = '';
 for(let i =0; i<allTheThings.length; i++) {
@@ -35,10 +26,4 @@ for(let i =0; i<allTheThings.length; i++) {
         } else break;
 
 } console.log(cell1)
-
-for (const i in allTheThings) {
-    if (allTheThings[i] != ',') {
-        console.log(allTheThings[i]);
-    } else break;
-}
 
